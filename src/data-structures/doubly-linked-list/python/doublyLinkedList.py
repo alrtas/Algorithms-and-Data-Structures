edc68d0fc50ad8b88e4502da04a32aae24cd3170
Thiago <alrrtas@gmail.com>
2020-05-31 22:34:02 -0300

    Minor updates

---
+++ src/data-structures/doubly-linked-list/python/doublyLinkedList.py
@@ -0,0 +1,114 @@
+class Node:
+    def __init__(self, data=None):  # it works
+        self.data = data
+        self.next = None
+        self.previous = None
+
+
+class DoublyLinkedList:
+    def __init__(self):  # it works
+        self.head = Node()
+        self.tail = Node()
+
+    def __del__(self):
+        current = self.head
+        current.next = None
+        current.previous = None
+
+    def append(self, data):  # it works
+        new_node = Node(data)
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+        new_node.next = self.tail
+        #new_node.previous = current
+        #new_node.next = self.tail
+
+    def prepend(self, data):  # it works
+        new_node = Node(data)
+        current = self.head
+        new_node.next = current.next
+        current.next = new_node
+        new_node.previous = current
+
+    def length(self):  # it works
+        current = self.head
+        total = 0
+        while current is not self.tail:
+            total += 1
+            current = current.next
+        return total-1  # discount the tail
+
+    def display(self):  # it works
+        elements = []
+        current = self.head
+        while current.next is not self.tail:
+            current = current.next
+            elements.append(current.data)
+        return elements
+
+    def displayBackwards(self):  # not works
+        elements = []
+        current = self.tail
+        while current.previous is not None:
+            current = current.previous
+        elements.append(current.data)
+        return elements
+
+    def path(self, index):  # it works
+        total = self.length()
+        path = total/2
+        if index > path:
+            return 0     # starts with tail
+        else:
+            return 1     # starts with head
+
+    def get(self, index):  # it works, just with path ==1
+        size = self.length()
+        if index > size:
+            return "ERROR: 'get' index out of range"
+        path = self.path(index)
+        if path == 1:
+            current_index = 0
+            current = self.head
+            while True:
+                current = current.next
+                if current_index == index:
+                    return current.data
+                current_index += 1
+        else:
+            current_index = size
+            current = self.tail
+            while True:
+                print current_index
+                current = current.previous
+                if current_index == index:
+                    return current.data
+                current_index -= 1
+
+    def erase(self, index):
+        size = self.length()
+        if index > size:
+            return "ERROR: 'get' index out of range"
+        path = self.path(index)
+        if path == 1:
+            current_index = 0
+            current = self.head
+            while True:
+                last_node = current
+                current = current.next
+                if current_index == index:
+                    last_node.next = current.next
+                    current.previous = last_node
+                current_index += 1
+        else:
+            current_index = size
+            current = self.tail
+            while True:
+                current = current.previous
+                next_node = current
+                if current_index == index:
+                    current.next = next_node.next
+                    next_node.previous = current
+                current_index -= 1
---
+++ src/data-structures/doubly-linked-list/python/main.py
@@ -0,0 +1,18 @@
+import doublyLinkedList
+
+my_list = doublyLinkedList.DoublyLinkedList()
+my_list.append(0)
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+
+
+print my_list.display()
+print my_list.displayBackwards()
+print my_list.length()
+print my_list.path(3)
+print my_list.get(6)
+
+
+
